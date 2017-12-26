0091496ba52a551781d0943233e31bdd7ce0a9af
zhengguang666 <582635492@qq.com>
2017-12-26 16:15:48 +0800

    add compile script \ solved huge difference between tx and rx

---
+++ dhd-linux-1.363.125.7/src/dhd/linux/adv.sh
@@ -0,0 +1,3 @@
+make ARCH=arm CROSS_COMPILE=/usr/local/cross_compiler/linux-devkit/sysroots/x86_64-arago-linux/usr/bin/arm-linux-gnueabihf- LINUXDIR=/home/zhengguang/work/7510/AM57XXLBB0040_2017-05-17/source/linux-4.4.19/ dhd-cdc-sdmmc-p2p-android-cfg80211 -oob
+
+#make ARCH=arm CROSS_COMPILE=/usr/local/cross_compiler/linux-devkit/sysroots/x86_64-arago-linux/usr/bin/arm-linux-gnueabihf- LINUXDIR=/home/zhengguang/work/7510/AM57XXLBB0040_2017-05-17/source/linux-4.4.19/ dhd-cdc-sdmmc-cfg80211 -oob 
